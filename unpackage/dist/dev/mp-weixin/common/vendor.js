0c547baa3682cdb70e80601336b3afb0c8c5f6ba
simon <ouyangyly@gmail.com>
2025-06-27 17:16:57 +0800

    更改了背景图，以及一部分话术

--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -7114,7 +7114,7 @@ function isConsoleWritable() {
 function initRuntimeSocketService() {
   const hosts = "172.21.108.23,127.0.0.1";
   const port = "8090";
-  const id = "mp-weixin_EKhI3o";
+  const id = "mp-weixin_WuGb50";
   const lazy = typeof swan !== "undefined";
   let restoreError = lazy ? () => {
   } : initOnError();
